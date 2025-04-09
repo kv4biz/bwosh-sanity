4f1abadaba84177168b4e9e8639b0ecab89ac0be
kv4biz <kv4biz@gmail.com>
2025-04-09 15:21:53 +0200

    editting layout correction3

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -17,9 +17,7 @@ const Page: React.FC = () => {
       <IntroSection
         image="https://res.cloudinary.com/dikzx4eyh/image/upload/v1735967643/Bwosh/about/aboutimg_fuarzp.jpg"
         imagePosition="left"
-        imagecontainer
-        px-2
-        lg:px-0ClassName="lg:hex-img-container3"
+        imagecontainerClassName="lg:hex-img-container3"
         title="Your Vision, Our Craftsmanship"
         description1="We are an interior design company, that was established in response to demands for creative, sustainable, functional and captivating spaces."
         description2="Our skilled, well-experienced and equipped team is bent on providing exceptional designs and excellent execution process through the lens of our clients' needs and wants with the drive to deliver exquisite products and services."
